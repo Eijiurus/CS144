f84ca1390355902f09ebc40c40ecfdf091acf986
Ruijie Su <suruijie0421@gmail.com>
2026-05-10 18:47:36 +0800

    check2 done

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,18 +1,44 @@
 #include "tcp_receiver.hh"
 #include "debug.hh"
+#include "wrapping_integers.hh"
+#include <cstdint>
+#include <optional>
 
 using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  // Your code here.
-  debug( "unimplemented receive() called" );
-  (void)message;
+  if ( message.RST ) {
+    reassembler_.reader().set_error();
+    return;
+  }
+
+  if ( message.SYN && !isn_.has_value() ) {
+    isn_ = message.seqno;
+  }
+
+  if ( !isn_.has_value() ) {
+    return;
+  }
+
+  auto checkpoint = reassembler_.writer().bytes_pushed() + 1;
+  auto abs_seqno = message.seqno.unwrap( isn_.value(), checkpoint );
+  auto first_index = abs_seqno - ( message.SYN ? 0 : 1 );
+
+  reassembler_.insert( first_index, message.payload, message.FIN );
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  // Your code here.
-  debug( "unimplemented send() called" );
-  return {};
+  std::optional<Wrap32> ackno;
+
+  if ( isn_.has_value() ) {
+    uint64_t abs_ack = reassembler_.writer().bytes_pushed() + 1;
+    abs_ack += reassembler_.writer().is_closed();
+    ackno = Wrap32::wrap( abs_ack, isn_.value() );
+  }
+
+  uint16_t windows_size = std::min<uint64_t>( reassembler_.writer().available_capacity(), UINT16_MAX );
+
+  return { ackno, windows_size, reassembler_.reader().has_error() };
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,18 +1,27 @@
 #include "wrapping_integers.hh"
 #include "debug.hh"
+#include <cstdint>
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return Wrap32( static_cast<uint32_t>( n ) + zero_point.raw_value_ );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  uint64_t MOD = ( 1ULL << 32 );
+
+  uint64_t offset = static_cast<uint32_t>( raw_value_ - zero_point.raw_value_ );
+  uint64_t base = checkpoint / MOD * MOD;
+  auto candidate = base + offset;
+
+  if ( candidate > checkpoint && candidate >= MOD && candidate - checkpoint > MOD / 2 ) {
+    candidate -= MOD;
+  } else if ( checkpoint > candidate && checkpoint - candidate > MOD / 2 ) {
+    candidate += MOD;
+  }
+
+  return candidate;
 }
